bd091aeb34b0be8f3cdb536653d5cfaa7faf3ac0
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-05-15 18:29:29 +0800

    Add files via upload

---
+++ src/scripts/views/pages/daftar.js
@@ -0,0 +1,13 @@
+const Daftar = {
+  async render() {
+    return `
+    <h2>Daftar Rumah Makan</h2>
+    `;
+  },
+
+  async afterRender() {
+
+  },
+};
+
+export default Daftar;
---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,13 @@
+const Detail = {
+  async render() {
+    return `
+    <h2>Detail Rumah Makan</h2>
+    `;
+  },
+
+  async afterRender() {
+
+  },
+};
+
+export default Detail;
