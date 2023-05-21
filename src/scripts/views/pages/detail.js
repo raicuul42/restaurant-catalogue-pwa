5209b0b6bf4d182c7cb3fc8c3c34bd5423bb8ea9
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-05-21 12:46:49 +0800

    Update detail.js

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,6 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantSource from '../../data/restaurant-source';
 import { createRestaurantDetailTemplate } from '../templates/template-creator';
-import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
@@ -16,16 +15,6 @@ const Detail = {
     const restaurants = await RestaurantSource.detail(url.id);
     const restaurantContainer = document.querySelector('.restaurant-detail');
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurants);
-
-    LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
-      restaurants: {
-        id: restaurants.id,
-        name: restaurants.name,
-        description: restaurants.description,
-        city: restaurants.city,
-        rating: restaurants.rating,
-      },
     });
   },
 };
